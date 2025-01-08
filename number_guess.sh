a2b5b532cc04bc608b3ef5bbf1329733874e1f69
Jürgen  Müller <jurgenmuller997@gmail.com>
2025-01-08 13:33:26 +0000

    test: Restructuring of variables and new query structure

--- number_guess.sh
+++ number_guess.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-
+# $PSQL "TRUNCATE users CASCADE"
 echo "Enter your username:"
 read USERNAME
 
-USER_SELECT=$($PSQL "SELECT user_id, username, games_played FROM users INNER JOIN game_stats USING(user_id) WHERE username ILIKE '$USERNAME'")
-echo "$USER_SELECT"
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
 
-USER_ID=$(echo "$USER_SELECT" | cut -d '|' -f 1)
-EXTRACTED_USERNAME=$(echo "$USER_SELECT" | cut -d '|' -f 2)
-
-if [[ -z $EXTRACTED_USERNAME ]]; then
-  EXTRACTED_USERNAME=$USERNAME
-fi
-
-USERNAME=$EXTRACTED_USERNAME
-
-if [[ -z $USER_SELECT ]]; then
-BEST_GAME=0
+if [[ -z $USER_ID ]]; then
+  BEST_GAME=0
 else
-BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) AS best_game FROM game_stats WHERE user_id = $USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) AS best_game FROM game_stats WHERE user_id=$USER_ID")
 fi
 
-GAMES_PLAYED=$(echo "$USER_SELECT" | cut -d '|' -f 3)
+GAMES_PLAYED=$($PSQL "SELECT COUNT(user_id) FROM game_stats WHERE user_id = 37")
 
 NUMBER_OF_GUESSES=0
 
@@ -45,10 +35,13 @@ NUMBER_GUESS() {
 
     if [[ $NUMBER -eq $SECRET_NUMBER ]]; then
       echo -e "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-      USER_SELECT=$($PSQL "SELECT user_id, username, games_played, MIN(number_of_guesses) AS best_game FROM users INNER JOIN game_stats USING(user_id) WHERE username ILIKE '$USERNAME'")
 
-      GAMES_PLAYED=$(($GAMES_PLAYED + 1))
-      $PSQL "INSERT INTO game_stats (user_id, games_played, number_of_guesses) VALUES ($USER_ID, $GAMES_PLAYED, $NUMBER_OF_GUESSES)"
+      if [[ -z $USER_ID ]]; then
+        $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+        USER_ID=$($PSQL "SELECT user_id FROM users WHERE username ILIKE '$USERNAME'")
+      fi
+
+      $PSQL "INSERT INTO game_stats (user_id, number_of_guesses) VALUES ($USER_ID, $NUMBER_OF_GUESSES)"
 
     elif [[ $NUMBER -gt $SECRET_NUMBER ]]; then
       NUMBER_GUESS "It's lower than that, guess again:"
@@ -60,35 +53,15 @@ NUMBER_GUESS() {
   fi
 }
 
-if [[ -z $USER_SELECT ]]; then
-
-  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
-  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username ILIKE '$USERNAME'")
-  # $PSQL "INSERT INTO game_stats(user_id) VALUES($USER_ID)"
-
-  #  USER_SELECT=$($PSQL "SELECT user_id, username, games_played, best_game FROM users INNER JOIN game_stats USING(user_id) WHERE username ILIKE '$USERNAME';")
-
-  #  echo "$USER_SELECT"
-
-  # USER_ID=$(echo "$USER_SELECT" | cut -d '|' -f 1)
-  #  USERNAME=$(echo "$USER_SELECT" | cut -d '|' -f 2)
-  #  GAMES_PLAYED=$(echo "$USER_SELECT" | cut -d '|' -f 3)
-  # BEST_GAME=$(echo "$USER_SELECT" | cut -d '|' -f 4)
-
-  # echo "USER_ID, $USER_ID"
-  # echo "USERNAME, $USERNAME"
-  # echo "GAMES_PLAYED, $GAMES_PLAYED"
-  # echo "BEST_GAME, $BEST_GAME"
-  # echo "NUMBER_OF_GUESSES, $NUMBER_OF_GUESSES"
-
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
-  NUMBER_GUESS
+if [[ -z $USER_ID ]]; then
+  NUMBER_GUESS "Welcome, $USERNAME! It looks like this is your first time here."
 else
+  : <<'EOF'
   echo "USER_ID, $USER_ID"
   echo "USERNAME, $USERNAME"
   echo "GAMES_PLAYED, $GAMES_PLAYED"
   echo "BEST_GAME, $BEST_GAME"
   echo "NUMBER_OF_GUESSES, $NUMBER_OF_GUESSES"
-  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-  NUMBER_GUESS
+EOF
+  NUMBER_GUESS "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
